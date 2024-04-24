2893a45c382001a685bfdd8c5066d7e23773beeb
Saturn Maoudis <saturnmaoudis@protonmail.com>
2024-04-24 17:49:37 +0100

    footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -24,8 +24,15 @@
 <nav
 	class="border-b bg-background text-foreground shadow-lg fixed top-0 w-full"
 >
-	<ul class="flex flex-row">
-		<li class="ml-auto">
+	<ul class="flex flex-row items-center [&>*]:p-2">
+		<li>
+			<a href="/">
+				<img class="w-[40px]" src="/favicon.png" alt="logo" />
+			</a>
+		</li>
+		<li><a href="/mods/explore">Mods</a></li>
+		<li><a href="/launcher">Launcher</a></li>
+		<li class="ml-auto h-[40]">
 			<DropdownMenu.Root>
 				<DropdownMenu.Trigger>
 					<Avatar.Root>
@@ -51,7 +58,9 @@
 						<DropdownMenu.Label>Account</DropdownMenu.Label>
 						<DropdownMenu.Separator />
 						{#if data.bearer !== undefined}
-							<DropdownMenu.Item>Mods</DropdownMenu.Item>
+							<DropdownMenu.Item href={`/mods/${data.username}`}
+								>My Mods</DropdownMenu.Item
+							>
 							<DropdownMenu.Separator />
 							<DropdownMenu.Item>Profile</DropdownMenu.Item>
 							<DropdownMenu.Item href="/auth/logout"
@@ -71,5 +80,114 @@
 
 <Toaster />
 
-<span class="block mb-[50px]" />
+<span class="block mb-[70px]" />
 <slot />
+
+<footer class="bg-background bottom-0">
+	<div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
+		<div class="lg:flex lg:items-start lg:gap-8">
+			<div
+				class="mt-8 grid grid-cols-2 gap-8 lg:mt-0 lg:grid-cols-5 lg:gap-y-16"
+			>
+				<div class="col-span-2 sm:col-span-1">
+					<p class="relative font-medium text-foreground">
+						Quick Access
+					</p>
+
+					<ul style="list-style:none;" class="mt-6 space-y-4 text-sm">
+						<li>
+							<a
+								href="/"
+								class="transition hover:opacity-75 text-foreground"
+							>
+								Home
+							</a>
+						</li>
+
+						<li>
+							<a
+								href="https://github.com/MiMLoader/"
+								class="transition hover:opacity-75 text-foreground"
+							>
+								GitHub
+							</a>
+						</li>
+
+						<li>
+							<a
+								href="https://discord.gg/KnT6eBJMuC"
+								class="transition hover:opacity-75 text-foreground"
+							>
+								Discord
+							</a>
+						</li>
+					</ul>
+				</div>
+
+				<div class="col-span-2 sm:col-span-1">
+					<p class="relative font-medium text-white">Tools</p>
+
+					<ul style="list-style:none;" class="mt-6 space-y-4 text-sm">
+						<li>
+							<a
+								href="https://github.com/mimloader/mimloader"
+								class="transition hover:opacity-75 text-foreground"
+							>
+								Mod Loader
+							</a>
+						</li>
+
+						<li>
+							<a
+								href="https://github.com/mimloader/mimlauncher"
+								class="transition hover:opacity-75 text-foreground"
+							>
+								Launcher
+							</a>
+						</li>
+
+						<li>
+							<a
+								href="https://github.com/mimloader/devkit"
+								class="transition hover:opacity-75 text-foreground"
+							>
+								DevKit
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+
+		<div class="mt-8 border-t pt-8 border-gray-800">
+			<div class="sm:flex sm:justify-between">
+				<p class="text-xs text-gray-400">
+					© 2024. MiMLoader. All rights reserved.
+				</p>
+
+				<ul
+					style="list-style:none;"
+					class="mt-8 flex flex-wrap justify-start gap-4 text-xs sm:mt-0 lg:justify-end"
+				>
+					<li>
+						<a
+							href="/tos"
+							class="transition hover:opacity-75 text-foreground"
+						>
+							Terms & Conditions
+						</a>
+					</li>
+
+					<li>
+						<a
+							href="/privacy"
+							class="transition hover:opacity-75 text-foreground"
+						>
+							Privacy Policy
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</footer>
---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,17 @@
+<p><strong>Privacy Policy</strong></p>
+<p>This Privacy Policy describes how we collect, use, and disclose information when you use our website and app. We are committed to protecting your privacy and ensuring that any information you provide to us is handled responsibly.</p>
+<p><strong>Information We Collect</strong></p>
+<p>We only collect Discord identification information when you use our website and app. This information may include your Discord username and user ID.</p>
+<p><strong>How We Use Your Information</strong></p>
+<p>We use the Discord identification information solely for the purpose of providing you with access to our website and app features that without needing to create an account with us. This may include features such as linking your Discord account for authentication.</p>
+<p><strong>Data Security</strong></p>
+<p>We take the security of your information seriously and have implemented appropriate technical and organizational measures to protect it against unauthorized access, alteration, disclosure, or destruction.</p>
+<p><strong>Third-Party Disclosure</strong></p>
+<p>We do not sell, trade, or otherwise transfer your Discord identification information to third parties. However, please note that our website and app may contain links to third-party websites or services, and we are not responsible for the privacy practices or content of those sites.</p>
+<p><strong>Children's Privacy</strong></p>
+<p>Our website and app are not directed to individuals under the age of 13, and we do not knowingly collect personal information from children under 13. If you are a parent or guardian and believe that your child has provided us with personal information, please contact us, and we will take appropriate steps to remove that information from our systems.</p>
+<p><strong>Changes to This Privacy Policy</strong></p>
+<p>We reserve the right to update or change this Privacy Policy at any time. Any changes will be effective immediately upon posting the revised Privacy Policy on this page.</p>
+<p><strong>Contact Us</strong></p>
+<p>If you have any questions or concerns about this Privacy Policy, please contact us at support@mimloader.com. </p>
+<p>This Privacy Policy was last updated on 24/4/2024.</p>
